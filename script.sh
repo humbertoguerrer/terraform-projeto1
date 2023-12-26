7dbd7b31b6df98a20fe4ee7d9969bce9fc4460d9
humberto.guerrer <humberto.guerrer@gmail.com>
2023-12-26 10:38:29 -0300

    subindo aplicacao conteinerizada

--- script.sh
+++ script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
-sudo apt install apache2 -y
-sudo systemctl start apache2
-git clone https://github.com/denilsonbonatti/mundo-invertido.git
-cd mundo-invertido
-sudo cp * -R /var/www/html
+
+sudo apt install curl -y
+
+curl -fsSL https://get.docker.com -o install-docker.sh
+sudo sh install-docker.sh
+
+sudo docker run -dti --name site-bootcamp -p 80:80 humbertoguerrer/meusite-bootcamp-devops:1.0
